c9e967547099c86efeacf10938a2fc31391818db
windymork7 <windymork7@naver.com>
2022-12-12 17:05:07 +0900

    class

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -46,13 +46,36 @@ function App() {
     );
   }
 
+  var classStyle = "color:red";
   class ClassComp extends Component{
     state = {
       number:this.props.initNumber,
       date:(new Date()).toString()
     }
 
+    componentWillMount(){
+      console.log("%cclass => componentWillMount", classStyle);
+    }
+
+    componentDidMount(){
+      console.log("%cclass => componentDidMount", classStyle);
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+      console.log("%cclass => shouldComponentUpdate", classStyle);
+      return true;
+    }
+
+    componentWillUpdate(nextProps, nextState){
+      console.log("%cclass => componentWillUpdate", classStyle);
+    }
+
+    componentDidUpdate(nextProps, nextState){
+      console.log("%cclass => componentDidUpdate", classStyle);
+    }
+
     render(){
+      console.log("%cclass => render", classStyle);
       return(
         <div className='container'>
           <h2>class style component</h2>
